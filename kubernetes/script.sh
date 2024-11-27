7c07200e17e806c776b3ef3054caf3f4abea045a
styveLioumba <styvelioumba@ov-c327f7.infomaniak.ch>
2024-11-27 23:26:42 +0000

    initialisation du cluster redis

--- kubernetes/script.sh
+++ kubernetes/script.sh
@@ -9,3 +9,12 @@ kubectl create service externalname redis-svc-bridge -n argocd --external-name r
 kustomize create --autodetect
 # deploiement via kustomize les manifests de dev
 kubectl create -k overlays/dev/
+
+# créer un cluster Redis à l'aide de l'outil CLI redis-cli
+kubectl exec -it -n redis deployments/dev-redis-creator -- redis-cli --cluster create $(kubectl get pods -n redis -o wide | grep redis-worker | awk '{print $6":6379"}' | tr '\n' ' ') --cluster-replicas 1
+
+# Vérifiez que tous les nœuds Redis sont correctement configurés comme maîtres et répliques dans le cluster
+kubectl exec -it -n redis deployments/dev-redis-creator -- redis-cli cluster nodes
+
+# Vérification que le worker est bien configuré
+kubectl exec -it -n redis statefulsets/dev-redis-workers  -- redis-cli ping
